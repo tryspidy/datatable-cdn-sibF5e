921eef04e1812be83a88aba0a3be52651b911219
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-15 11:18:38 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// css
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">  
+// Js
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
+// call jquey
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
+// jquery Google cdn
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
